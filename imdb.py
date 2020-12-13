ac8eadbc23a7d82d5dc7b51a8a997c686f48c7ee
Tshebeletso <t.tjabane@gmail.com>
2020-12-13 11:56:51 +0200

    imdb example

---
+++ imdb.py
@@ -0,0 +1,38 @@
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.python.keras.metrics import acc
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+history = model.fit(partial_x_train, partial_y_train, epochs=4, batch_size=512, validation_data=(x_val, y_val))
+results = model.evaluate(x_test, y_test)
+
+print(results)
+
+
+
